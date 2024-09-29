e4592590f4c2c0fa78dac2b644079c49e44b2e99
jai-2211 <jairajput504@gmail.com>
2024-09-29 13:08:02 +0530

    Add files via upload

---
+++ Day-1 perimeter of square.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+ #include <stdio.h>
+
+ int main()
+ {
+   int a , perimeter;
+   printf("enter the value of a:");
+   scanf("%d", & a);
+    perimeter = (4*a);
+    printf("the perimeter of square is :%d\n",perimeter);
+    
+    return 0;
+ }    
